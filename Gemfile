cadc709852e6a75bd2bdb8d18dabaa74b6adaedc
AnthonyBobsin <bobsinj@gmail.com>
2015-04-07 13:14:08 -0400

    Mailer config

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
+  gem 'foreman'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,18 @@ Rails.application.configure do
 
   # Devise Mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: ENV['GMAIL_DOMAIN'],
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD']
+  }
 end
